1b30f190731b64ac977a93b5a62b211da0d4efd7
edwardnie <nieyichao03@kuaishou.com>
2020-07-27 20:49:37 +0800

    feat: 不打断校验

--- demos/javascript/from实现.js
+++ demos/javascript/from实现.js
@@ -1,2 +1,25 @@
+/*
+ * @Author: edwardnie
+ * @LastEditTime: 2020-07-27 20:49:22
+ * @LastEditors: edwardnie
+ * @FilePath: /diamond_zhang/demos/javascript/from实现.js
+ */
+
 //我还没学明白from
 //over
+
+console.log(Array.from("foo"));
+// expected output: Array ["f", "o", "o"]
+
+console.log(Array.from([1, 2, 3], (x) => x + x));
+// expected output: Array [2, 4, 6]
+
+Array.myfrom = function () {
+    console.log("%c  么么哒，爱你哟", "color:red;");
+    debugger;
+    let arr = [];
+    Array.prototype.forEach.call(arguments, function (item) {
+        arr.push(item);
+    });
+    return arr;
+};
--- demos/javascript/javascript.md
+++ demos/javascript/javascript.md
@@ -1,6 +1,6 @@
 <!--
  * @Author: edwardnie
- * @LastEditTime: 2020-07-26 15:32:07
+ * @LastEditTime: 2020-07-27 20:39:32
  * @LastEditors: edwardnie
  * @FilePath: /diamond_zhang/demos/javascript/javascript.md
 --> 
@@ -15,4 +15,10 @@ this指向问题案例
 快排、冒泡、插入、选择
 jsonp实现
 ajax实现
-cookie封装
+cookie封装, get, set,delete ,clear方法。
+Object的 assign, freeze, keys 实现。
+用call实现bind
+封装函数，依次调用之前的异步promise, 例如：    [promise1, promise2, 3]  =>   promise1 -> promise2 -> 3
+合并两个无序数组
+合并两个有序数组
+数组去重:  [{a:1},{a:2},{a:3}, 1,2,3,3,{a:2}] 
