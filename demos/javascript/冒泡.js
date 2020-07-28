48a20c321a6bd5af337269356fdaa09cf8a9f44e
edwardnie <nieyichao03@kuaishou.com>
2020-07-28 16:55:55 +0800

    afa

--- demos/javascript/冒泡.js
+++ demos/javascript/冒泡.js
@@ -1,14 +1,19 @@
-function myBubbleSort(array){
-    const len=array.length;
-    if(len<=1){
+/*
+ * @Author: edwardnie
+ * @LastEditTime: 2020-07-28 16:54:40
+ * @LastEditors: edwardnie
+ * @FilePath: /diamond_zhang/demos/javascript/冒泡.js
+ */
+
+function myBubbleSort(array) {
+    const len = array.length;
+    if (len <= 1) {
         return array;
     }
-    for(let i=len;i>0;i--){
-        for(let j=0;j<i;j++){
-            if(array[j]>array[j+1]){
-                const buffer=array[j];
-                array[j]=array[j+1];
-                array[j+1]=buffer;
+    for (let i = len; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
             }
         }
     }
