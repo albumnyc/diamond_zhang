865b144231996fab8412dfb788679a3becda747c
mochalhui <964618207@qq.com>
2020-07-28 22:52:06 +0800

    wuwuwu

--- demos/javascript/数组去重.js
+++ demos/javascript/数组去重.js
@@ -1,2 +1,19 @@
 //简单对象去重，不考虑深度对象
-//bye
+function mySimple(arr){
+    const newArr = [];
+    const obj = {};
+    arr.forEach(item => {
+        if (typeof item !== 'object') {
+            if (newArr.indexOf(item) === -1) {
+                newArr.push(item);
+            }
+        } else {
+            const str = JSON.stringify(item);
+            if (!obj[str]) {
+                newArr.push(item);
+                obj[str] = 1;
+            }
+        }
+    })
+    return newArr;
+}
