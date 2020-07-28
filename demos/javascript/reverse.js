9062bacb7a83ad2b56faa6112dac21ce5bbdee0a
edwardnie <nieyichao03@kuaishou.com>
2020-07-28 21:18:12 +0800

    asdasd

---
+++ demos/javascript/reverse.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: edwardnie
+ * @LastEditTime: 2020-07-28 21:17:58
+ * @LastEditors: edwardnie
+ * @FilePath: /diamond_zhang/demos/javascript/reverse.js
+ */ 
+const str = " zhangjiahui     like  nieyichao  very       very much ";
+function reserve(str) {
+
+}
+reserve(str); // -> " much very       very  nieyichao  like     zhangjiahui "
