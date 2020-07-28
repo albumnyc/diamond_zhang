61c9d885dba48cf7d4b6e400cdf047e9044233cb
edwardnie <nieyichao03@kuaishou.com>
2020-07-28 16:25:53 +0800

    afa

--- demos/javascript/快排.js
+++ demos/javascript/快排.js
@@ -1,17 +1,27 @@
+/*
+ * @Author: edwardnie
+ * @LastEditTime: 2020-07-28 16:25:38
+ * @LastEditors: edwardnie
+ * @FilePath: /diamond_zhang/demos/javascript/快排.js
+ */ 
+
+/**
+ * @description:  快排
+ * @param {type} Array<number>
+ * @return:  Array<number>
+ */ 
 function myQuickSort(array) {
-    if(array.length<=1){
+    const len = array.length;
+    if (len <= 1) {
         return array;
     }
-    const pivot=array[0],left=[],right=[];
-    for(let i=1;i<array.length;i++){
-        const buffer=array[i];
-        if(buffer>=pivot){
-            right.push(buffer);
-        }
-        if(buffer<pivot){
-            left.push(buffer);
-        }
+    const pivot = array[0];
+    const  left = [];
+    const right = [];
+    
+    for (let i = 1; i < len; i++) {
+        const buffer = array[i];
+        buffer >= pivot ? right.push(buffer) : left.push(buffer);
     }
-    return myQuickSort(left).concat(pivot,myQuickSort(right));
-
+    return myQuickSort(left).concat(pivot, myQuickSort(right));
 }
