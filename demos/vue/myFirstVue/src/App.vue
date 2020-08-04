f01b8439e0f3430688364de758d200454b7de5d4
mochalhui <964618207@qq.com>
2020-08-04 17:04:11 +0800

    我的拼图他不出现了

---
+++ demos/vue/myFirstVue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <h1>{{title_msg}}</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      title_msg: '拼图冲一冲'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color:darkseagreen;
+  margin-top: 60px;
+}
+</style>
---
+++ demos/vue/todo.md
@@ -0,0 +1,4 @@
+1、vue-cli学习使用
+2、用vue-cli生成项目，并且在gitgnore屏蔽掉node_modules
+3、使用scss开发
+4、完成拼图小游戏
